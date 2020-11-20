a92d161cc0babfad8aced61ebea5a55db91a7937
liusong <1141185877@qq.com>
2020-11-20 17:42:29 +0800

    try finally中return测试

---
+++ src/main/java/com/example/concurrent/demo/ReturnAndFinalDemo.java
@@ -0,0 +1,22 @@
+package com.example.concurrent.demo;
+
+/**
+ * @author: ls
+ * @date: 2020/11/20 14:40
+ * 测试return和final先后
+ **/
+public class ReturnAndFinalDemo {
+
+    public static void main(String[] args) {
+        System.out.println(test());
+    }
+
+    static int test(){
+        try {
+            //多个return会被finally覆盖
+            return 1;
+        }finally {
+            return 2;
+        }
+    }
+}
